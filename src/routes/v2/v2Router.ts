5eea9788128dd69ddf72649f6264e696d10b27e0
Tim Aagaard <tim.aagaard@gmail.com>
2025-04-07 21:58:05 -0400

    Revised v2 routes.

--- src/routes/v2/v2Router.ts
+++ src/routes/v2/v2Router.ts
@@ -2,14 +2,13 @@ import express from "express";
 
 export const v2Router = express.Router();
 
-v2Router.get("/", (_req, res) => {
-    res.send({ message: "v2 working" }).status(200);
-});
-v2Router.get("/cities");
-v2Router.get("/city/distance");
-v2Router.get("/countries");
-v2Router.get("/country");
-v2Router.get("/country/distance");
-v2Router.get("/flag/image");
-v2Router.get("/random");
-v2Router.get("/states");
+v2Router.get("/");
+v2Router.get("/:countryIdOrName");
+v2Router.get("/:countryIdOrName/states");
+v2Router.get("/:countryIdOrName/state/:stateIdOrName");
+v2Router.get("/:countryIdOrName/state/:stateIdOrName/cities");
+v2Router.get("/:countryIdOrName/state/:stateIdOrName/city/:cityIdOrName");
+v2Router.get("/between/:lat1/:lon1/:lat2/:lon2");
+v2Router.get("/cities/between/:lat1/:lon1/:lat2/:lon2");
+v2Router.get("/within/:distanceAmount/:distanceUnit");
+v2Router.get("/cities/within/:distanceAmount/:distanceUnit");
