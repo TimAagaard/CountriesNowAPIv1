85f5be5248e1df627b9221bfa6b84a1f6c026113
Tim Aagaard <tim.aagaard@gmail.com>
2025-04-08 15:40:04 -0400

    Created controller method for v2 GET /cities/between/:lat1/:lon1/:lat2/:lon2 route.  Closes #50.

--- src/controllers/v2Controller.ts
+++ src/controllers/v2Controller.ts
@@ -14,6 +14,42 @@ export const v2Controller = {
         res.status(200).send(response);
     },
 
+    getCitiesBetweenLatitudeAndLongitude: async (
+        req: Request,
+        res: Response,
+    ) => {
+        const { lat1, lat2, lon1, lon2 } = req.params;
+
+        const latitudes = [parseFloat(lat1), parseFloat(lat2)];
+        const longitudes = [parseFloat(lon1), parseFloat(lon2)];
+
+        const minLat = Math.min(...latitudes);
+        const maxLat = Math.max(...latitudes);
+
+        const minLon = Math.min(...longitudes);
+        const maxLon = Math.max(...longitudes);
+
+        const results = await prisma.city.findMany({
+            where: {
+                AND: [
+                    {
+                        latitude: {
+                            gte: minLat,
+                            lte: maxLat,
+                        },
+                        longitude: {
+                            gte: minLon,
+                            lte: maxLon,
+                        },
+                    },
+                ],
+            },
+        });
+        results.sort((a, b) => a.name.localeCompare(b.name));
+        const response = new APIResponse(results).success();
+        res.status(200).send(response);
+    },
+
     getCitiesByCountryAndState: async (req: Request, res: Response) => {
         const { countryIdOrName, stateIdOrName } = req.params;
         const results = await prisma.state.findMany({
@@ -104,6 +140,7 @@ export const v2Controller = {
                 ],
             },
         });
+        results.sort((a, b) => a.name.localeCompare(b.name));
         const response = new APIResponse(results).success();
         res.status(200).send(response);
     },
